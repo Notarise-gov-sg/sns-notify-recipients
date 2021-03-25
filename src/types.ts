938cc8c7383687b5c2af9db6d94ebb0f09fffc1d
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-03-25 15:21:10 +0800

    Import SNS implementation from api-notarise-formsg

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+// TODO: Obtain types from a single source of truth?
+export interface TestData {
+  provider: string;
+  lab: string;
+  swabType: string;
+  patientName: string;
+  swabCollectionDate: string;
+  performerName: string;
+  performerMcr: string;
+  observationDate: string;
+  nric: string;
+  passportNumber: string;
+  birthDate: string;
+  testType: string;
+  nationality: string;
+  gender: string;
+  testResult: string;
+}
