c77ef24d5aec88e159267b1e96887bec59bd791e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-04-20 16:11:48 +0800

    Export notifyVaccine

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { notifyPdt } from "./models/notifyPdt";
+import { notifyVaccine } from "./models/notifyVaccine";
 import { healthCheck } from "./util/healthCheck";
 
-export { notifyPdt, healthCheck };
+export { notifyPdt, notifyVaccine, healthCheck };
