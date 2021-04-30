62dc63bf1f7529bca12b2f045cedb72fe44fc467
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-04-30 16:20:37 +0800

    Set expiry to "Not Applicable"

--- src/models/notifyVaccine/notifyVaccine.test.ts
+++ src/models/notifyVaccine/notifyVaccine.test.ts
@@ -79,7 +79,7 @@ describe("notifyVaccine", () => {
                 "template_input": Object {
                   "name": "Person",
                   "qrcode": "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAIQAAACECAYAAABRRIOnAAAAAklEQVR4AewaftIAAAN2SURBVO3BQa7cVgADweaD7n/ljhdZcCVAGP2x47Aq/sLMvw4z5TBTDjPlMFMOM+UwUw4z5TBTDjPlMFMOM+UwUw4z5TBTDjPl4kNJ+CaVO0loKneS0FRaEppKS0JTaUn4JpVPHGbKYaYcZsrFy1TelIQnVFoS7qg8kYRPqLwpCW86zJTDTDnMlIsfloQnVJ5Iwh2VloSWhKbyTUl4QuUnHWbKYaYcZsrFXy4JTyTh/+wwUw4z5TBTLv4yKk8k4Y7KnST8TQ4z5TBTDjPl4oepfFMSmsoTKneS8CaVP8lhphxmymGmXLwsCb+TSktCU3kiCU2lJeGJJPzJDjPlMFMOM+XiQyr/ZSo/SeW/5DBTDjPlMFMuPpSEptKS8CaVptKS0FSeSMITKneS8CaVn3SYKYeZcpgpFx9SaUloKi0JT6i8KQl3VN6k0pLQVJ5IQlN502GmHGbKYabEX/hAEprKm5LQVL4pCXdUWhKayp0k3FFpSbij8onDTDnMlMNMufhhSXhCpancSUJTaUloKk+ovCkJTeUJlZaENx1mymGmHGbKxQ9TaUloKi0Jd1R+pyQ8kYSm0pLQVFoSvukwUw4z5TBTLl6WhKbyCZWWhDtJaCp3knBHpam0JDSVO0loKi0JTaUloam86TBTDjPlMFPiL3wgCU2lJeGbVD6RhDeptCQ8odKScEflE4eZcpgph5ly8cNUnkhCU/lEEppKS8IdlZaEptKS8AmVOyotCW86zJTDTDnMlIsvS8ITSWgqLQlN5RMqLQl3knAnCZ9IQlNpKm86zJTDTDnMlIsPqTyh8idRuaPSknBH5YkktCQ0lW86zJTDTDnMlIsPJeGbVJrKE0m4o3JHpSXhThKayh2VloQnVD5xmCmHmXKYKRcvU3lTEu4koak8oXInCU3lCZUnktBU7iThTYeZcpgph5ly8cOS8ITKm1RaEp5QaUm4k4RPqLQkfNNhphxmymGmXPzPqLQkvEmlJaGptCS0JPxOh5lymCmHmXLxP6fSkvCEyh2VOyotCXdUWhLedJgph5lymCkXP0zlJ6l8Igl3VFoSWhLuqLQkPKHyTYeZcpgph5ly8bIkfFMS7qg8odKS0FS+KQnfdJgph5lymCnxF2b+dZgph5lymCmHmXKYKYeZcpgph5lymCmHmXKYKYeZcpgph5lymCn/AGM6fxRnfplxAAAAAElFTkSuQmCC",
-                  "qrexpiryts": "15 April 2021",
+                  "qrexpiryts": "Not Applicable",
                   "qrissuedts": "1 April 2021",
                   "travelpassport": "E7831177G",
                   "vaccdate": "1 April 2021",
