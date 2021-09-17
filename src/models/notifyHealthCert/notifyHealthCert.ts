e89e5dc336c3446110daaaedee217a6611ad31b6
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-09-17 08:53:59 +0800

    feat: add new SNS publish function for SPM wallet (#108)

--- src/models/notifyHealthCert/notifyHealthCert.ts
+++ src/models/notifyHealthCert/notifyHealthCert.ts
@@ -4,13 +4,17 @@ import { getLogger } from "../../util/logger";
 const { trace } = getLogger("src/models/notifyHealthCert");
 
 interface NotifyHealthCertProps {
+  version: string;
+  type: string;
   url: string;
-  expiry?: number;
+  expiry: number;
 }
 
-export const notifyHealthCert = async ({ url, expiry }: NotifyHealthCertProps) => {
+export const notifyHealthCert = async ({ version, type, url, expiry }: NotifyHealthCertProps) => {
   const { MessageId } = await publish({
     notification_req: {
+      version,
+      type,
       url,
       expiry,
     },
