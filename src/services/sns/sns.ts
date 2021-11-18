316c13ffd0f5e10b94191a89d7d2136346a9b6ee
Zaw Myo Latt <zawmyolatt.ucsy@gmail.com>
2021-11-18 11:23:38 +0800

    fix: remove logs for sns payload

--- src/services/sns/sns.ts
+++ src/services/sns/sns.ts
@@ -1,25 +1,23 @@
 import * as AWSTypes from "aws-sdk";
 import { config } from "../../config";
-import { getLogger } from "../../util/logger";
 import { AWS } from "../awsSdk";
 
-const { trace } = getLogger("src/services/sns");
 const sns = new AWS.SNS(config.notification.sns) as AWSTypes.SNS;
 
 export const publish = (message: any) => {
-  const publishMessage = {
-    Message: JSON.stringify(message),
-    TopicArn: config.notification.topicArn,
-  };
-  trace(`Publishing Notification : ${JSON.stringify(publishMessage)}`);
-  return sns.publish(publishMessage).promise();
+  return sns
+    .publish({
+      Message: JSON.stringify(message),
+      TopicArn: config.notification.topicArn,
+    })
+    .promise();
 };
 
 export const publishHealthCert = (message: any) => {
-  const publishMessage = {
-    Message: JSON.stringify(message),
-    TopicArn: config.notification.healthCertTopicArn,
-  };
-  trace(`Publishing Healthcert Notification : ${JSON.stringify(publishMessage)}`);
-  return sns.publish(publishMessage).promise();
+  return sns
+    .publish({
+      Message: JSON.stringify(message),
+      TopicArn: config.notification.healthCertTopicArn,
+    })
+    .promise();
 };
